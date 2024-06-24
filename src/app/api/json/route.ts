b7e8af475b2eb6eacb2f021718d6c66e3e358af1
HillaryG1 <hillgilbert7gmail.com>
2024-06-24 16:51:46 -0400

    Implement Zod

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,7 +1,21 @@
 import { NextRequest } from "next/server";
+import { z } from "zod";
+
+const jsonSchemaToZod = (schema: any) => {};
 
 export const POST = async (req: NextRequest) => {
   const body = await req.json();
+
+  // 1. Ensure incoming request is valid
+  const genericSchema = z.object({
+    data: z.string(),
+    format: z.object({}).passthrough(),
+  });
+
+  const { data, format } = genericSchema.parse(body);
+
+  // 2. Create a schema from the expected user format
+  const dynamicSchema = jsonSchemaToZod(format);
 
   return new Response("OK");
 };
